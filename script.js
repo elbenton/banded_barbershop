1a42e1e707e54661725f7f904c2ee37e58f5045c
elbenton <54687566+elbenton@users.noreply.github.com>
2020-01-21 13:58:19 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+    let feed = new Instafeed({
+        get: 'user',
+        userId: '11825143043',
+        template: '<a href="{{link}}"><img class="insta-image" src="{{image}}" /></a>',
+        accessToken: '11825143043.1677ed0.a22281bd420e43f2b1fdf21a33182975'
+    });
+    feed.run();
+
+$( document ).ready(function() {
+    initialHandlers();
+});
